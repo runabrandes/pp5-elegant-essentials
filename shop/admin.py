c8062451307c967d20cda481e25b4b52f5f324c7
Runa Brandes <runabrandes@gmail.com>
2024-09-26 19:35:23 +0000

    Created and registered product model

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name',)
+    search_fields = ['product_name']
+    list_filter = ('product_name',)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    different_sizes = models.BooleanField(default=False, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.product_name
