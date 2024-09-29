092c7bca596bd7407456ddcc606330a28cf08f90
Runa Brandes <runabrandes@gmail.com>
2024-09-29 18:14:10 +0000

    Added Category model and added products for shop

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 
 # Register your models here.
 
@@ -9,3 +9,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name',)
     search_fields = ['product_name']
     list_filter = ('product_name',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name',)
+    search_fields = ['category_name']
+    list_filter = ('category_name',)
--- shop/models.py
+++ shop/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category_name
+
+
 class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     product_name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -11,5 +19,9 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(blank=True, null=True)
 
+    class Meta:
+        ordering = ['product_name']
+
     def __str__(self):
         return self.product_name
+
