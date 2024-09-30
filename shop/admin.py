2ffda5e9ba02462c5b525640ef131f5ce886ce2f
Runa Brandes <runabrandes@gmail.com>
2024-09-30 17:08:55 +0000

    First set up for product rendering

--- shop/admin.py
+++ shop/admin.py
@@ -6,7 +6,7 @@ from .models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name',)
+    list_display = ('product_name', 'price',)
     search_fields = ['product_name']
     list_filter = ('product_name',)
 
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from .models import Product
 
 def shop(request):
     """
-    This view renders index.html
+    This view renders shop.html
     """
-    return render(request, 'shop/shop.html')
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'shop/shop.html', context)
