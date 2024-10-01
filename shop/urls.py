d3badb0d6027dbbfb2912b619959cca17b62d215
Runa Brandes <runabrandes@gmail.com>
2024-10-01 18:36:41 +0000

    Implemented product_info.html and added noimage.png

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shop, name='shop') 
+    path('', views.shop, name='shop'),
+    path('<product_id>', views.product_info, name='product_info')
 ]
--- shop/views.py
+++ shop/views.py
@@ -14,3 +14,15 @@ def shop(request):
     }
 
     return render(request, 'shop/shop.html', context)
+
+
+def product_info(request, product_id):
+    """ A view to show individual product information """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'shop/product_info.html', context)
