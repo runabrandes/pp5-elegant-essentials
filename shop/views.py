2ed75c0a32d2da0dbd7415d775d655916ee0c27d
Runa Brandes <runabrandes@gmail.com>
2024-10-02 14:51:37 +0000

    Added functionality to category nav bar for hair and skin products

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.views import generic
-from .models import Product
+from .models import Product, Category
 from django.db.models import Q
 
 
@@ -12,8 +12,14 @@ def shop(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category']
+            products = products.filter(category__category_name__icontains=categories)
+            categories = Category.objects.filter(category_name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -26,6 +32,7 @@ def shop(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'shop/shop.html', context)
