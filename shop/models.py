40df319a349f88388f76015390cbb0749e23eb5c
Runa Brandes <runabrandes@gmail.com>
2024-10-02 14:03:39 +0000

    Added friendly name to shop.models for filtering categories

--- shop/models.py
+++ shop/models.py
@@ -4,10 +4,14 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.category_name
 
+    def get_friendly_name(self):
+        return self.friendly_name
+        
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
