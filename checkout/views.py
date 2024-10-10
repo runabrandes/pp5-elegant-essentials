41603f188197cdb8d4757adc5f032eace177c953
Runa Brandes <runabrandes@gmail.com>
2024-10-10 20:44:04 +0000

    Set up Stripe payment element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Q8SnMAn8cKANS5IDFzDDT6wnykoYqv1PXfw2MvbbVe3xfxyS7SwDXxJOeyZF7tAY4lul46p9IUNR7XAptL7ITjH00XEgm2KWm',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
