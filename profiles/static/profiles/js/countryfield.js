81959f751fc8d3004115f71d5d8ace39c3522501
Runa Brandes <runabrandes@gmail.com>
2024-10-19 17:31:13 +0000

    Added form for profile info update and enabled messages to show on website

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,12 @@
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#aab7c4');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#464a4e');
+    } else {
+        $(this).css('color', '#4a0374');
+    }
+});
