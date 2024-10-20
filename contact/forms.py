4547de04051a242c061eb80ecc6a11d312ad4807
Runa Brandes <runabrandes@gmail.com>
2024-10-20 15:47:15 +0000

    Created contact.html and form for users to send requests to the store

---
+++ contact/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    """
+    Generates the contact request form for users so they can input needed information.
+    """
+    class Meta:
+        model = Contact
+        fields = (
+            'name',
+            'email',
+            'comment')
+        labels = {
+            'name': 'Name',
+            'email': 'Email',
+            'comment': 'Comment',
+        }
