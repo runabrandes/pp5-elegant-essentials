47b17b29c3c86e6603707c5e01fddb3543026bd2
Runa Brandes <runabrandes@gmail.com>
2024-10-21 18:16:54 +0000

    Adjusted views for FAQ and created html template

--- FAQ/views.py
+++ FAQ/views.py
@@ -3,5 +3,5 @@ from .models import FAQ
 
 # Create your views here.
 def faq_q_and_a(request):
-    faq_page = FAQ.objects.all()
-    return render(request, 'FAQ/FAQ.html', {faq_page: faq_page})
+    faqs_page = FAQ.objects.all()
+    return render(request, 'FAQ/FAQ.html', {'faqs_page': faqs_page})
