54139fb7b762d7da5ce30b1053ae4b4115ef9e4c
Runa Brandes <runabrandes@gmail.com>
2024-10-21 17:30:42 +0000

    Created model and views for FAQ page

--- FAQ/views.py
+++ FAQ/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import FAQ
 
 # Create your views here.
+def faq_q_and_a(request):
+    faq_page = FAQ.objects.all()
+    return render(request, 'FAQ/FAQ.html', {faq_page: faq_page})
