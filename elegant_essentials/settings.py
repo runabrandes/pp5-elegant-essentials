8e230751d94582633179b65c44d0eff960c1327f
Runa Brandes <runabrandes@gmail.com>
2024-10-24 18:24:22 +0000

    Adjusted Django version

--- elegant_essentials/settings.py
+++ elegant_essentials/settings.py
@@ -192,9 +192,8 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
-
-
-
+else:
+    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
