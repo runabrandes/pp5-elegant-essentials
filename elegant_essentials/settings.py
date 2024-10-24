388bfba2700ec96f8c68cbaff4bb321a96d46cc9
Runa Brandes <runabrandes@gmail.com>
2024-10-24 18:30:34 +0000

    Added else block for static_root in USE_AWS settings

--- elegant_essentials/settings.py
+++ elegant_essentials/settings.py
@@ -169,12 +169,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if 'USE_AWS' in os.environ:
+if os.environ.get('USE_AWS') == 'True':
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'elegant-essentials'
     AWS_S3_REGION_NAME = 'eu-north-1'
@@ -192,7 +192,9 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
-    
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
