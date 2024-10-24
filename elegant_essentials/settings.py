eb386678f3d2f37b6fbf6f5858e597da1c9caaa3
Runa Brandes <runabrandes@gmail.com>
2024-10-24 17:49:40 +0000

    Set debug to false

--- elegant_essentials/settings.py
+++ elegant_essentials/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-runabrandes-pp5elegante-22s6c8233xi.ws.codeinstitute-ide.net',
