706eb85e9454ec047e809f229f1272cd53efc0aa
Runa Brandes <runabrandes@gmail.com>
2024-10-24 18:27:16 +0000

    Removed else statement from settings.py

--- elegant_essentials/settings.py
+++ elegant_essentials/settings.py
@@ -192,7 +192,6 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
-else:
     
 
 # Default primary key field type
