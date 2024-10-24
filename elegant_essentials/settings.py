ceccd95d6784afdb99f3e8e1c6bbc94cf3ec251f
Runa Brandes <runabrandes@gmail.com>
2024-10-24 20:05:46 +0000

    Added cache control for AWS

--- elegant_essentials/settings.py
+++ elegant_essentials/settings.py
@@ -175,6 +175,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if os.environ.get('USE_AWS') == 'True':
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'elegant-essentials'
     AWS_S3_REGION_NAME = 'eu-north-1'
