94bfe027591641dbf29717c6230b406e873efd02
Runa Brandes <runabrandes@gmail.com>
2024-10-28 19:39:06 +0000

    Updated checkout views to assign userprofile to order as required

--- checkout/views.py
+++ checkout/views.py
@@ -57,6 +57,11 @@ def checkout(request):
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
+
+            if request.user.is_authenticated:
+                profile = UserProfile.objects.get(user=request.user)
+                order.user_profile = profile
+
             pid = request.POST.get('client_secret').split('_secret')[0]
             order.stripe_pid = pid
             order.original_bag = json.dumps(bag)
@@ -115,7 +120,7 @@ def checkout(request):
                 'street_address1': profile.default_street_address1,
                 'street_address2': profile.default_street_address2,
                 'county': profile.default_county,
-                })
+            })
         except UserProfile.DoesNotExist:
             order_form = OrderForm()
     else:
@@ -142,39 +147,38 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    if order.user_profile.user == request.user:
-        if request.user.is_authenticated:
-            profile = UserProfile.objects.get(user=request.user)
-            order.user_profile = profile
-            order.save()
+    if order.user_profile:
+        if order.user_profile.user == request.user:  
+            if request.user.is_authenticated:
+                profile = UserProfile.objects.get(user=request.user)
 
-            if save_info:
-                profile_data = {
-                    'default_phone_number': order.phone_number,
-                    'default_country': order.country,
-                    'default_postcode': order.postcode,
-                    'default_town_or_city': order.town_or_city,
-                    'default_street_address1': order.street_address1,
-                    'default_street_address2': order.street_address2,
-                    'default_county': order.county,
-                }
-                user_profile_form = UserProfileForm(profile_data, instance=profile)
-                if user_profile_form.is_valid():
-                    user_profile_form.save()
+                if save_info:
+                    profile_data = {
+                        'default_phone_number': order.phone_number,
+                        'default_country': order.country,
+                        'default_postcode': order.postcode,
+                        'default_town_or_city': order.town_or_city,
+                        'default_street_address1': order.street_address1,
+                        'default_street_address2': order.street_address2,
+                        'default_county': order.county,
+                    }
+                    user_profile_form = UserProfileForm(profile_data, instance=profile)
+                    if user_profile_form.is_valid():
+                        user_profile_form.save()
 
-        messages.success(request, f'Order successfully processed! \
-            Your order number is {order_number}. A confirmation \
-            email will be sent to {order.email}.')
+            messages.success(request, f'Order successfully processed! \
+                Your order number is {order_number}. A confirmation \
+                email will be sent to {order.email}.')
 
-        if 'bag' in request.session:
-            del request.session['bag']
+            if 'bag' in request.session:
+                del request.session['bag']
 
-        template = 'checkout/checkout_success.html'
-        context = {
-            'order': order,
-        }
+            template = 'checkout/checkout_success.html'
+            context = {
+                'order': order,
+            }
 
-        return render(request, template, context)
-    else:
-        messages.error(request, 'You are not allowed to view the order you are trying to access.')
-        return redirect('home')
+            return render(request, template, context)
+
+    messages.error(request, 'You are not authorised to view the order confirmation you are trying to access.')
+    return redirect('home')
